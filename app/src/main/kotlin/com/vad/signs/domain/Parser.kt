290a463a7d317dfae972dbca9d2c77641d9dbdc3
Liem Vo <liemvt@gmail.com>
2022-01-08 14:36:41 +1300

    update parser

--- app/src/main/kotlin/com/vad/signs/domain/Parser.kt
+++ app/src/main/kotlin/com/vad/signs/domain/Parser.kt
@@ -8,63 +8,65 @@ import com.vad.signs.domain.models.Sign
 import com.vad.signs.domain.models.SignGroup
 import java.io.IOException
 
-
-fun Context.assetsToBitmap(fileName: String): Bitmap? {
-    return try {
-        with(assets.open(fileName)) {
-            BitmapFactory.decodeStream(this)
-        }
-    } catch (e: IOException) {
-        null
-    }
-}
-
-fun ByteArray.toBitmap(): Bitmap? {
-    return try {
-        BitmapFactory.decodeByteArray(this, 0, this.size)
-    } catch (exception: Exception) {
-        null
-    }
-}
-
-suspend fun Context.parserSign(): List<SignGroup> {
-    val result = mutableListOf<SignGroup>()
-    try {
-        val inputStream = assets.open("data/signs.csv")
-        val bufferedReader = inputStream.bufferedReader()
-        var line: String?
-        var group: SignGroup? = null
-        var signs = mutableListOf<Sign>()
-
-        while (bufferedReader.readLine().run {
-                line = this
-                this != null
-            } && line != null) {
-            val notNullLine = line!!
-            if (notNullLine.startsWith('1')) {
-                if (group != null) {
-                    result.add(group)
-                }
-                signs = mutableListOf()
-                val title = notNullLine.split("|")[2]
-                group = SignGroup(title, signs)
-            } else {
-                val texts = notNullLine.split("|")
-                val sign = Sign(
-                    texts[1],
-                    texts[2],
-                    description = texts[3],
-                    texts[4]
-                )
-                signs.add(sign)
-                group = group?.copy(signs = signs)
+object Parser {
+    fun assetsToBitmap(context: Context, fileName: String): Bitmap? {
+        return try {
+            with(context.assets.open(fileName)) {
+                BitmapFactory.decodeStream(this)
             }
+        } catch (e: IOException) {
+            null
         }
-        if (group != null && !result.contains(group)) {
-            result.add(group)
-        }
-    } catch (exception: Exception) {
-        Log.e("Parser", "error copy file", exception)
     }
-    return result
+
+    fun toBitmap(byteArray: ByteArray): Bitmap? {
+        return try {
+            BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
+        } catch (exception: Exception) {
+            null
+        }
+    }
+
+    suspend fun parserSign(context: Context): List<SignGroup> {
+        val result = mutableListOf<SignGroup>()
+        try {
+            val inputStream = context.assets.open("data/signs.csv")
+            val bufferedReader = inputStream.bufferedReader()
+            var line: String?
+            var group: SignGroup? = null
+            var signs = mutableListOf<Sign>()
+
+            while (bufferedReader.readLine().run {
+                    line = this
+                    this != null
+                } && line != null) {
+                val notNullLine = line!!
+                if (notNullLine.startsWith('1')) {
+                    if (group != null) {
+                        result.add(group)
+                    }
+                    signs = mutableListOf()
+                    val title = notNullLine.split("|")[2]
+                    group = SignGroup(title, signs)
+                } else {
+                    val texts = notNullLine.split("|")
+                    val sign = Sign(
+                        texts[1],
+                        texts[2],
+                        description = texts[3],
+                        texts[4]
+                    )
+                    signs.add(sign)
+                    group = group?.copy(signs = signs)
+                }
+            }
+            if (group != null && !result.contains(group)) {
+                result.add(group)
+            }
+        } catch (exception: Exception) {
+            Log.e("Parser", "error copy file", exception)
+        }
+        return result
+    }
+
 }
--- buildSrc/src/main/kotlin/Publishing.kt
+++ buildSrc/src/main/kotlin/Publishing.kt
@@ -11,7 +11,7 @@ object Maven {
             setProperty("user", "unspecified")
             setProperty("key", "unspecified")
             setProperty("build", "1")
-            setProperty("version", "0.6")
+            setProperty("version", "0.7")
         }
     }
 
@@ -41,6 +41,6 @@ object Maven {
     var version  = if (localProperties.contains("version")) {
         localProperties.getProperty("version").trim()
     } else {
-        "0.6"
+        "0.7"
     }
 }
